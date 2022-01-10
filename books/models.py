030b33e8af79189b4ba727dae371a9c71726838a
mohamed <hamood_shaheen@outlook.com>
2022-01-10 03:11:50 +0200

    book models design

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.db.models.fields import CharField
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
+
+
+
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=100, help_text=_("The book title"), verbose_name=_("Title"))
+    added_dt = models.DateTimeField(auto_now_add=True, verbose_name=_("Added at"))
