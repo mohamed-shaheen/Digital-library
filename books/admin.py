ecb1512e5f94c62e03e8a3de915aba6ea22eaede
mohamed <hamood_shaheen@outlook.com>
2022-01-18 01:56:02 +0200

    category and rating models

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,11 @@
 from atexit import register
 import site
 from django.contrib import admin
-from .models import Book
+from .models import Book, Category, Rating
 # Register your models here.
 
 
 
 admin.site.register(Book)
+admin.site.register(Category)
+admin.site.register(Rating)
--- books/models.py
+++ books/models.py
@@ -1,11 +1,13 @@
 from re import T
+from unicodedata import category
 from django.db import models
 from django.db.models.fields import CharField
 from django.utils.translation import gettext_lazy as _
 from django.utils.text import slugify
 from django.urls import reverse
 from django.contrib.auth.models import User
-from django.core.validators import RegexValidator, FileExtensionValidator
+from django.core.validators import RegexValidator, FileExtensionValidator, MinValueValidator, MaxValueValidator
+from django.db.models import CheckConstraint, Q, UniqueConstraint
 # Create your models here.
 
 
@@ -23,7 +25,8 @@ class Book(models.Model):
     publisher = models.CharField(max_length=100, blank=True, null=True, verbose_name=_("Publisher"))
     publish_dt = models.DateField(verbose_name=_("Published at"))
     description = models.TextField(max_length=1000, blank=True, null=True, verbose_name=_("Description"))
-    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_("Uploaded by"))
+    category = models.ForeignKey("Category", blank=True, null=True, on_delete=models.CASCADE, related_name="book_cat", verbose_name=_("Category"))
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="book_user", verbose_name=_("Uploaded by"))
     cover_img = models.ImageField(upload_to="cover_img/", blank=True, null=True, verbose_name=_("Cover image"))
     file = models.FileField(upload_to=user_directory_path, validators=[FileExtensionValidator(['pdf'], message=_("the file must be '.pdf'."))], verbose_name=_("Book file"))
     slug = models.SlugField(blank=True, null=True, verbose_name=_("Slug"), allow_unicode=True)
@@ -44,3 +47,53 @@ class Book(models.Model):
     def __str__(self):
         return self.title[:15]
 
+
+
+class Category(models.Model):
+
+    title = models.CharField(max_length=60, verbose_name=_("Title"))
+    description = models.TextField(max_length=400, blank=True, null=True, verbose_name=_("Description"))
+    img = models.ImageField(upload_to="Category_img/", width_field="200", height_field="200", blank=True, null=True, verbose_name=_("Image"))
+    created_dt = models.DateTimeField(auto_now_add=True, verbose_name=_("Created at"))
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_("Created by"))
+    slug = models.SlugField(blank=True, null=True, allow_unicode=True, verbose_name=_("Slug"))
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title, allow_unicode=True)
+        super(Category, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("categories")
+
+    def __str__(self):
+        return self.title[:10]
+
+    #def get_absolute_url(self):
+    #    return reverse('\', kwargs={'pk': self.pk}) 
+
+
+
+class Rating(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="rating_user", verbose_name=_("User"))
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="rating_book", verbose_name=_("Book"))
+    rating = models.DecimalField(max_digits=3, decimal_places=1, validators=[MinValueValidator(0.0), MaxValueValidator(10.0)], verbose_name=_("Rating") )
+    rating_dt = models.DateTimeField(auto_now_add=True, verbose_name=_("Rating at"))
+
+
+    def __str__(self):
+        return str(self.rating)
+
+    class Meta:
+        verbose_name = _("Rating")
+        verbose_name_plural = _("Ratings")
+        constraints = [
+            CheckConstraint(check=Q(rating__range=(0, 10)), name='valid_rate'),
+            UniqueConstraint(fields=['user', 'book'], name='rating_once')
+        ]
+
+    #def get_absolute_url(self):
+    #    return reverse('\', kwargs={'pk': self.pk})  
