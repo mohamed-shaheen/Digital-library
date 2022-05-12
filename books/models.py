9c3c29735b33a74c15322efd7a23a742a199e5a2
mohamed <hamood_shaheen@outlook.com>
2022-05-12 15:24:52 +0200

    edit book detail

--- books/models.py
+++ books/models.py
@@ -80,7 +80,7 @@ class Rating(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="rating_user", verbose_name=_("User"))
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="rating_book", verbose_name=_("Book"))
-    rating = models.DecimalField(max_digits=3, decimal_places=1, validators=[MinValueValidator(0.0), MaxValueValidator(10.0)], verbose_name=_("Rating") )
+    rating = models.DecimalField(max_digits=3, decimal_places=1, validators=[MinValueValidator(0.0), MaxValueValidator(5.0)], verbose_name=_("Rating") )
     rating_dt = models.DateTimeField(auto_now_add=True, verbose_name=_("Rating at"))
 
 
@@ -91,7 +91,7 @@ class Rating(models.Model):
         verbose_name = _("Rating")
         verbose_name_plural = _("Ratings")
         constraints = [
-            CheckConstraint(check=Q(rating__range=(0, 10)), name='valid_rate'),
+            CheckConstraint(check=Q(rating__range=(0, 5)), name='valid_rate'),
             UniqueConstraint(fields=['user', 'book'], name='rating_once')
         ]
 
--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fontawesomefree',
     'books',
     'django_summernote',
     'django_filters',
