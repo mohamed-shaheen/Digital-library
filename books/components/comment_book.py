135c82d8173ceebce3618dda7c3166d66b4127a8
mohamed <hamood_shaheen@outlook.com>
2022-08-03 02:03:45 +0200

    edit

--- books/components/comment_book.py
+++ books/components/comment_book.py
@@ -1,20 +1,36 @@
-from books.views import book_detail
 from django_unicorn.components import UnicornView
 from books.models import Comment, Book
-from django.shortcuts import get_object_or_404
+
 
 
 
 class CommentBookView(UnicornView):
     
 
-    comment = ""
+    comment_post = ""
     bookid: int = 0
+    comments: Comment = None
+    book: Book = None
+    
+    def mount(self):
+        self.comments = Comment.objects.all()
+        return super().mount()
 
- 
-    def comments_v(self):
-        if self.bookid:
-            return []    
-        print(self.bookid) 
-        comments = Comment.objects.all().filter(post_book_id__exact=int(self.bookid))
-        return comments
+        
+    def comments_v(self, num=0):
+        if num > 0:
+           self.bookid=num
+        self.book = Book.objects.get(id=self.bookid)
+        Comment.objects.create(
+            post_by=self.request.user,
+            post_book=self.book,
+            post=self.comment_post
+        )
+        print(self.bookid)
+        self.comment_post= ""
+        self.comments = Comment.objects.filter(post_book_id__exact=self.book.id)
+
+
+
+    def book_v(self):
+        return self.comments    
